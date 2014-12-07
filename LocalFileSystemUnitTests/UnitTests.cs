22291e6e82329e8fb5ec63fec2921b1c9222db5d
blady92 <blady992@gmail.com>
2014-12-07 23:20:58 +0100

    local file systeminterface and it's implementation

---
+++ LocalFileSystemUnitTests/UnitTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using LFS;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private IFileSystem fileSystem = new LocalFileSystem();
+
+        [TestMethod]
+        public void TestMethodGetFile()
+        {
+            string currDir = System.Environment.CurrentDirectory;
+            FileInfo expected = new FileInfo(currDir);
+            FileInfo result = fileSystem.GetFile(currDir);
+            Assert.AreEqual(expected.FullName, result.FullName);
+        }
+
+        [TestMethod]
+        public void TestMethodGetFiles()
+        {
+            string currDir = System.Environment.CurrentDirectory;
+            string explorer = "C:\\Windows\\explorer.exe";
+            DirectoryInfo expected = new DirectoryInfo(currDir);
+            List<string> transformedExpectedList = new List<string>();
+            foreach (FileInfo finfo in expected.GetFiles())
+            {
+                transformedExpectedList.Add(finfo.FullName);
+            }
+            List<string> transformedResultList = new List<string>();
+            foreach (FileInfo finfo in fileSystem.GetFiles(currDir))
+            {
+                transformedResultList.Add(finfo.FullName);
+            }
+            CollectionAssert.AreEquivalent(transformedExpectedList, transformedResultList);
+            transformedExpectedList.Clear();
+            transformedExpectedList.Add(explorer);
+            transformedResultList.Clear();
+            foreach (FileInfo finfo in fileSystem.GetFiles(explorer))
+            {
+                transformedResultList.Add(finfo.FullName);
+            }
+            CollectionAssert.AreEqual(transformedExpectedList, transformedResultList);
+        }
+
+        [TestMethod]
+        public void TestMethodCopyMoveAndDelete()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            if (Directory.Exists(tempDirectory))
+            {
+                Directory.Delete(tempDirectory);
+            }
+            Directory.CreateDirectory(tempDirectory);
+            string currDir = System.Environment.CurrentDirectory;
+            fileSystem.Copy(currDir, tempDirectory, fileSystem);
+            DirectoryInfo expected = new DirectoryInfo(currDir);
+            DirectoryInfo result = new DirectoryInfo(tempDirectory);
+            long expectedSize = 0, resultSize = 0;
+            foreach (FileInfo finfo in expected.GetFiles())
+            {
+                expectedSize += finfo.Length;
+            }
+            foreach (FileInfo finfo in result.GetFiles())
+            {
+                resultSize += finfo.Length;
+            }
+            Assert.AreEqual(expectedSize, resultSize);
+
+            string secondTempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            if (Directory.Exists(secondTempDirectory))
+            {
+                Directory.Delete(secondTempDirectory);
+            }
+            Directory.CreateDirectory(secondTempDirectory);
+            fileSystem.Move(tempDirectory, secondTempDirectory, fileSystem);
+            expected = new DirectoryInfo(currDir);
+            result = new DirectoryInfo(secondTempDirectory);
+            expectedSize = 0; resultSize = 0;
+            foreach (FileInfo finfo in expected.GetFiles())
+            {
+                expectedSize += finfo.Length;
+            }
+            foreach (FileInfo finfo in result.GetFiles())
+            {
+                resultSize += finfo.Length;
+            }
+            Assert.AreEqual(expectedSize, resultSize);
+            Assert.IsFalse(Directory.Exists(tempDirectory));
+
+            fileSystem.Remove(secondTempDirectory);
+            Assert.IsFalse(Directory.Exists(secondTempDirectory));
+        }
+    }
+}
---
+++ TotalCommander/LocalFileSystem/IFileSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFS
+{
+    public interface IFileSystem
+    {
+        void Copy(String from, String to, IFileSystem fileSystem);
+        void Move(String from, String to, IFileSystem fileSystem);
+        FileInfo GetFile(String path);
+        List<FileInfo> GetFiles(String path);
+        void Remove(String path);
+        void Write(FileInfo file, string path);
+        void Write(DirectoryInfo dir, string path);
+    }
+}
