a9bc2e4264371e0cf909fbdf8a7386e7d3bf151f
MKxx <xtheskx@gmail.com>
2014-12-07 20:32:26 +0100

    SSH complete

---
+++ TotalCommander/SSH/SSHController.cs
@@ -0,0 +1,64 @@
+﻿using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalCommander.SSH
+{
+    public class SSHController : IFileSystem
+    {
+        SftpClient sftp;
+
+        public SSHController(){
+        }
+
+        public void connect(String address, String user, String password)
+        {
+            sftp = new SftpClient(address, user, password);
+            sftp.Connect();
+        }
+
+        public void copy(string from, string to)
+        {
+            using (var file = File.OpenRead(from))
+            {
+                sftp.UploadFile(file, to);
+            }
+        }
+
+        public FileInfo getFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<FileInfo> getFiles(string path)
+        {
+            List<FileInfo> dirContent = new List<FileInfo>();
+            var files = sftp.ListDirectory(path);
+            foreach (var file in files)
+            {
+                dirContent.Add(new FileInfo(file.FullName));
+            }
+            return dirContent;
+        }
+
+        public void move(string from, string to)
+        {
+            sftp.Get(from).MoveTo(to);
+        }
+
+        public void remove(string path)
+        {
+            sftp.Delete(path);
+        }
+
+        public void rename(string path, string name)
+        {
+            sftp.RenameFile(path, name);
+        }
+    }
+}
