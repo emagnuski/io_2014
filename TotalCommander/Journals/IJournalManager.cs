5a6914d6f0ffe19f5c4ea639017e21dd9910bb64
blady92 <blady992@gmail.com>
2014-12-28 15:00:55 +0100

    journal and abstract manager

---
+++ TotalCommander/Journals/IJournalManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalCommander.Journals
+{
+    interface IJournalManager
+    {
+        public List<Journal> GetJournals();
+        public void RemoveJournal(int journalNumber);
+    }
+
+    public class JournalManagerImpl : IJournalManager
+    {
+        private List<Journal> journals;
+
+        public JournalManagerImpl()
+        {
+            journals = new List<Journal>();
+            JournalsLimit = 10;
+            GenerationInterval = 3600;
+        }
+
+        public int JournalsLimit
+        {
+            get;
+            set;
+        }
+
+        public long GenerationInterval
+        {
+            get;
+            set;
+        }
+
+        public List<Journal> GetJournals()
+        {
+            return new List<Journal>(journals);
+        }
+
+        public void RemoveJournal(int journalNumber)
+        {
+            journals.RemoveAt(journalNumber);
+        }
+
+        public abstract void GenerateJournal();
+    }
+}
---
+++ TotalCommander/Journals/Journal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalCommander.Journals
+{
+    class Journal
+    {
+        private String id;
+        private Dictionary<String, String> attributes;
+        private DateTime creationDate;
+
+        public static readonly String extension = ".jou";
+
+        public Journal(String id, Dictionary<String, String> attributes)
+        {
+            this.id = id;
+            this.attributes = attributes;
+            this.creationDate = DateTime.Now;
+        }
+
+        public void Serialize(String path)
+        {
+            if (Directory.Exists(path))
+            {
+                path = Path.Combine(path, id + extension);
+            }
+            FileStream fs = File.OpenWrite(path);
+            StreamWriter writer = new StreamWriter(fs);
+            writer.WriteLine("Journal id " + id);
+            writer.WriteLine(creationDate);
+            writer.WriteLine();
+            foreach (String key in attributes.Keys)
+            {
+                writer.WriteLine(key + ": " + attributes[key]);
+            }
+        }
+
+        public String Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public Dictionary<String, String> Attributes
+        {
+            get
+            {
+                Dictionary<String, String> attrbs = new Dictionary<string,string>(attributes);
+                return attrbs;
+            }
+        }
+
+        public DateTime CreationDate
+        {
+            get
+            {
+                return creationDate;
+            }
+        }
+    }
+}
