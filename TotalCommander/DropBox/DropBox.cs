79530ba990c5d7b9ca2a752c9bde7fb27f4f353b
unknown <mariuszstasiak90@gmail.com>
2015-01-03 20:28:02 +0100

    Dodanie modulu DropBox

---
+++ TotalCommander/DropBox/DropBox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DropNet.Models;
+using DropNet;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TotalCommander.DropBox
+{
+    class DropBox : IFileSystem
+    {
+        DropNetClient client;
+
+        public DropBox()
+        {
+        }
+
+        public void connect()
+        {
+            //trzeba bedzie wygenerowac dla wlasciwej aplikacji
+            client = new DropNetClient("l1eott9plu8q24s", "o5z985cqb6avzfp", "asl6k3cej6rbyeuy", "56j4g0z8chb5rvb");
+        }
+
+        public void CopyFile(string from, string to)
+        {
+            client.Copy(from, to);
+        }
+
+        public byte[] GetFile(string path)
+        {
+            return client.GetFile(path);
+        }
+
+        public void MoveFile(string from, string to)
+        {
+            client.Move(from, to);
+        }
+
+        public void DeleteFile(string path)
+        {
+            client.Delete(path);
+        }
+
+        public void RenameFile(string path, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string path, string fileName, byte[] fileData)
+        {
+            client.UploadFile(path, fileName, fileData);
+        }
+
+        public void CreateFolder(string path)
+        {
+            client.CreateFolder(path);
+        }
+
+    }
+}
---
+++ TotalCommander/DropBox/IFileSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalCommander.DropBox
+{
+    interface IFileSystem
+    {
+        void CopyFile(String from, String to);
+        byte[] GetFile(String path);
+        void MoveFile(String from, String to);
+        void DeleteFile(String path);
+        void RenameFile(String path, String name);
+        void UploadFile(String path, String fileName, byte[] fileData);
+        void CreateFolder(String name);
+
+    }
+}
